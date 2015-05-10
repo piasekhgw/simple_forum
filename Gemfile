bf6f42eba6c0212330a6913aa037734b64c63f5a
piasekhgw <wojciech.piasecki@netguru.pl>
2015-05-10 19:31:06 +0200

    rom-rb gems and pry

--- Gemfile
+++ Gemfile
@@ -6,4 +6,8 @@ gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
+gem 'rom', '~> 0.6.1'
+gem 'rom-rails', '~> 0.3.0'
+gem 'rom-sql', '~> 0.4.1'
+gem 'pry', '~> 0.10.1'
 
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module SimpleForum
   class Application < Rails::Application
+    require 'rom-rails'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
